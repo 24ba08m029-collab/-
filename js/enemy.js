2728258a79026d46fdf9f4bfb79e625af4c4e566
24ba08m029-collab <24ba08m029@g.jbedu.kr>
2026-07-09 12:20:37 +0900

    Add enemy data structure with initial enemies

---
+++ js/enemy.js
@@ -0,0 +1,30 @@
+const enemies = [
+  {
+    id: 1,
+    name: "슬라임",
+    x: 300,
+    y: 200,
+    width: 32,
+    height: 32,
+    hp: 30,
+    maxHp: 30,
+    attack: 5,
+    exp: 10,
+    gold: 5,
+    color: "green"
+  },
+  {
+    id: 2,
+    name: "고블린",
+    x: 500,
+    y: 250,
+    width: 32,
+    height: 32,
+    hp: 60,
+    maxHp: 60,
+    attack: 10,
+    exp: 20,
+    gold: 15,
+    color: "darkgreen"
+  }
+];
