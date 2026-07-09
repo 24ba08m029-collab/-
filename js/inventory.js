59d7b62df932bc308a1e7c3fdfe9de4b31f07fad
24ba08m029-collab <24ba08m029@g.jbedu.kr>
2026-07-09 12:22:50 +0900

    Add initial inventory items to inventory.js

---
+++ js/inventory.js
@@ -0,0 +1,22 @@
+const inventory = [
+    {
+        name: "빨간 포션",
+        type: "consumable",
+        heal: 50
+    },
+    {
+        name: "파란 포션",
+        type: "consumable",
+        mp: 30
+    },
+    {
+        name: "나무검",
+        type: "weapon",
+        attack: 5
+    },
+    {
+        name: "나무방패",
+        type: "shield",
+        defense: 3
+    }
+];
