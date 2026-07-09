bf84f575aa45bd844197c0abdb9d73b27a119ca1
24ba08m029-collab <24ba08m029@g.jbedu.kr>
2026-07-09 12:19:10 +0900

    Add player.js

---
+++ js/player.js
@@ -0,0 +1,29 @@
+const player = {
+    x: 100,
+    y: 100,
+
+    width: 32,
+    height: 32,
+
+    speed: 4,
+
+    hp: 100,
+    maxHp: 100,
+
+    mp: 50,
+    maxMp: 50,
+
+    level: 1,
+    exp: 0,
+    nextExp: 10,
+
+    gold: 0,
+
+    attack: 10,
+    defense: 5,
+
+    inventory: [
+        "빨간 포션",
+        "나무검"
+    ]
+};
